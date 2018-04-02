da4aaa94149e43b9def0fb12710e8d067cb7bd76
Ryan Rauch <rauch.ryan@gmail.com>
2018-04-01 20:25:39 -0500

    Add project files.

---
+++ ApproxiMATEwebApi/Services/IEmailSender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApproxiMATEwebApi.Services
+{
+    public interface IEmailSender
+    {
+        Task SendEmailAsync(string email, string subject, string message);
+    }
+}
