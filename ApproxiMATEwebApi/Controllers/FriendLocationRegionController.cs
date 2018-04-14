239415df359df6e5c734bd4abc65a49ba6383ece
Ryan Rauch <rauch.ryan@gmail.com>
2018-04-13 19:53:17 -0500

    initial locationregion controller

---
+++ ApproxiMATEwebApi/Controllers/FriendLocationRegionController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ApproxiMATEwebApi.Data;
+using ApproxiMATEwebApi.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApproxiMATEwebApi.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/FriendLocationRegion")]
+    [Authorize]
+    public class FriendLocationRegionController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public FriendLocationRegionController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PostFriendLocationRegion([FromBody] FriendLocationRegionRequest region)
+        {
+            if(!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            var applicationUser = _context.ApplicationUser
+                                          .SingleOrDefault(a => a.Id == region.UserId.ToString());
+            if (applicationUser == null)
+            {
+                return NotFound(region.UserId);
+            }
+            var applicationOptioins = await _context.ApplicationOptions
+                                                    .OrderByDescending(a => a.OptionsDate)
+                                                    .FirstOrDefaultAsync();
+            if(applicationOptioins == null)
+            {
+                return NotFound("ApplicationOptions");
+            }
+            var locationRegion = new FriendLocationRegion
+            {
+                RegionID = region.RegionId,
+                Friends = new List<FriendLocationResult>()
+            };
+
+            //TODO: Implement Ray-casting algorithm
+            //      to determine if users are within the specific region
+            return NotFound("splution for region polygons");
+
+            return Ok(locationRegion);
+        }
+    }
+}
---
+++ ApproxiMATEwebApi/Models/FriendLocationRegionRequest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApproxiMATEwebApi.Models
+{
+    public class FriendLocationRegionRequest
+    {
+        public Guid UserId { get; set; }
+        public int RegionId { get; set; }
+    }
+}
